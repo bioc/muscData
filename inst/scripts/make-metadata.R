d6789c35e16f81800455b329fa5ef34707faf468
HelenaLC <helena@crowells.eu>
2019-06-08 13:13:04 +0200

    rmv Sala19 dataset

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,4 +1,4 @@
-ids <- c("Kang18_8vs8", "Sala19_2vs2")
+ids <- c("Kang18_8vs8")
 
 Kang18_8vs8 <- data.frame(
     stringsAsFactors = FALSE,
@@ -21,29 +21,6 @@ Kang18_8vs8 <- data.frame(
     RDataClass = "SingleCellExperiment",
     DispatchClass = "Rda",
     RDataPath = "muscData/Kang18_8vs8.Rda")
-
-Sala19_2vs2 <- data.frame(
-    stringsAsFactors = FALSE,
-    Title = "Sala19_2vs2",
-    Description = paste(
-        "10x scRNA-seq data from chimeric mice embryos", 
-        "in which Tal1-knockout (KO) embryonic stem cells", 
-        "were injected into a wild-type (WT) blastocyst.", 
-        "Represented as a SingleCellExperiment; derived from data",
-        "made available by the Cancer Research UK Cambridge Institute."),
-    BiocVersion = "3.9",
-    Genome = NA,
-    SourceType = "tar.gz",
-    SourceUrl = "https://content.cruk.cam.ac.uk/jmlab/chimera_tal1_data",
-    SourceVersion = "Feb 02 2018",
-    Species = "Mus musculus",
-    TaxonomyId = "10090",
-    Coordinate_1_based  = NA,
-    DataProvider = "CRUK",
-    Maintainer = "Helena L. Crowell <helena.crowell@uzh.ch>",
-    RDataClass = "SingleCellExperiment",
-    DispatchClass = "Rda",
-    RDataPath = "muscData/Sala19_2vs2.Rda")
 
 # write to .csv
 df <- do.call("rbind", lapply(ids, get))
